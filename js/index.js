5ac1317848d21ecb350a4640ba9e3a37acf9758e
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-04-30 13:22:15 -0400

    Made the Add and Remove collection buttons work properly!

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+// import * as roster from './roster.js';
 const addChar = document.getElementById('roster');
 const eleFilter = document.getElementById('e-filter');
 const pathFilter = document.getElementById('p-filter');
 const addReset = document.getElementById('addReset');
+const addCollection = document.getElementById('addCollection');
+const subCollection = document.getElementById('subCollection');
+const collection = document.getElementById('collection');
 pathFilter.addEventListener('change',popDropdowns);
 eleFilter.addEventListener('change',popDropdowns);
 addReset.addEventListener('click',reset);
+addCollection.addEventListener('click',popCollection);
+subCollection.addEventListener('click',remCollection);
 elements.forEach(element => {
     const option = document.createElement('option');
     option.setAttribute('value', element);
@@ -30,6 +36,23 @@ function popDropdowns(event){
             addChar.appendChild(option);
         }
     });
+}
+function popCollection(){
+    console.log(collection.children.length);
+    for(let x = 0;x<collection.children.length;x++)
+    {
+        if(collection.children[x].value === addChar.value) return;
+    }
+    const option = document.createElement('option');
+    option.setAttribute('value', addChar.value);
+    option.textContent = addChar.value;
+    collection.appendChild(option);
+}
+function remCollection(){
+    for(let x = 0;x<collection.children.length;x++)
+    {
+        if(collection.children[x].value === collection.value) collection.removeChild(collection.children[x]);
+    }
 }
 function reset(event){
     event.preventDefault();
